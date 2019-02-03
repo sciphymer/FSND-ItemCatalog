2465ca8ffca4d69fa2a394a8a3c5a1b2391ae01b
Vincent Ng <vincentnstng@gmail.com>
2019-02-03 19:43:03 +0800

    init Commit, setup the format of DB objects

---
+++ dbModels.py
@@ -0,0 +1,51 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from userModel import User
+
+Base = declarative_base()
+
+
+class Category(Base):
+	__tablename__ = 'categories'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(80), nullable=False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+
+	@property
+	def serialize(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+		}
+
+
+class Item(Base):
+	__tablename__ = 'items'
+
+	id = Column(Integer, primary_key=True)
+	title = Column(String(80), nullable=False)
+	description = Column(String(250), nullable=True)
+	cat_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'id': self.id,
+			'title': self.title,
+			'description': self.description,
+			'cat_id': self.cat_id
+		}
+
+
+engine = create_engine('sqlite:///sportCategories.db')
+Base.metadata.create_all(engine)
---
+++ userModel.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+
+Base = declarative_base()
+
+
+class User(Base):
+	__tablename__ = 'users'
+
+	id = Column(Integer, primary_key=True)
+	username = Column(String(32), index=True)
+	password_hash = Column(String(64))
+
+	# User Info while login with OAuth
+	name = Column(String(250), nullable=False)
+	email = Column(String(250), nullable=True)
+	picture = Column(String(250), nullable=True)
+
+	def hash_password(self, password):
+		self.password_hash = pwd_context.encrypt(password)
+
+	def verify_password(self, password):
+		return pwd_context.verify(password, self.password_hash)
+
+	@property
+	def serialize(self):
+		return{
+			self.username,
+			self.name,
+			self.email,
+			self.picture
+		}
+
+
+engine = create_engine('sqlite:///users.db')
+Base.metadata.create_all(engine)
